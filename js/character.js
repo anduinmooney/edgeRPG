b9da4e5dc622efcc262f18948ad61142d7a6f09a
Anduin Mooney <anduinmooney@gmail.com>
2018-02-08 15:30:02 -0800

    Add species prototype.

--- js/character.js
+++ js/character.js
@@ -1,5 +1,5 @@
 export class Character {
-  constructor(brawn, agility, intelligence, cunning, willpower, precense, soak, wound, strain, defense, astrogation, athletics, charm, coercion, computers, cool, coordination, deception, discipline, leadership, mechanics, medicine, negotiation, perception, pilotingPlanetary, pilotingSpace, resillience, skulduggery, stealth, streetwise, survival, vigilance, brawl, gunnary, melee, rangedLight, rangedHeavy, coreWorlds, education, lore, outerRim, underworld, xenology, weapon, armor, credits, xp) {
+  constructor(brawn, agility, intelligence, cunning, willpower, precense, soak, wound, strain, defense, astrogation, athletics, charm, coercion, computers, cool, coordination, deception, discipline, leadership, mechanics, medicine, negotiation, perception, pilotingPlanetary, pilotingSpace, resillience, skulduggery, stealth, streetwise, survival, vigilance, brawl, gunnary, melee, rangedLight, rangedHeavy, coreWorlds, education, lore, outerRim, underworld, xenology, weapon, armor, credits, xp, species) {
     this.brawn = brawn;
     this.agility = agility;
     this.intelligence = intelligence;
@@ -47,4 +47,342 @@ export class Character {
     this.armor = armor;
     this.credits = credits;
     this.xp = xp;
+    this.species = species;
   }
+
+  checktype() {
+    if(this.species === "bothan") {
+      let brawn = 1;
+      let agility = 2;
+      let intelligence = 2;
+      let cunning = 3;
+      let willpower = 2;
+      let precense = 2;
+      let xp = 100;
+
+  } else if (this.species === "droid") {
+    let brawn = 1;
+    let agility = 1;
+    let intelligence = 1;
+    let cunning = 1;
+    let willpower = 1;
+    let precense = 1;
+    let xp = 175;
+
+  } else if (this.species === "gand") {
+    let brawn = 2;
+    let agility = 2;
+    let intelligence = 2;
+    let cunning = 2;
+    let willpower = 3;
+    let precense = 1;
+    let xp = 100;
+
+  } else if (this.species === "human") {
+    let brawn = 2;
+    let agility = 2;
+    let intelligence = 2;
+    let cunning = 2;
+    let willpower = 2;
+    let precense = 2;
+    let xp = 110;
+
+  } else if (this.species === "rodian") {
+    let brawn = 2;
+    let agility = 3;
+    let intelligence = 2;
+    let cunning = 2;
+    let willpower = 1;
+    let precense = 2;
+    let xp = 100;
+
+  } else if (this.species === "trandoshan") {
+    let brawn = 3;
+    let agility = 1;
+    let intelligence = 2;
+    let cunning = 2;
+    let willpower = 2;
+    let precense = 2;
+    let xp = 90;
+
+  } else if (this.species === "twi'lek") {
+    let brawn = 1;
+    let agility = 2;
+    let intelligence = 2;
+    let cunning = 2;
+    let willpower = 2;
+    let precense = 3;
+    let xp = 100;
+
+  } else if (this.species === "wookiee") {
+    let brawn = 3;
+    let agility = 2;
+    let intelligence = 2;
+    let cunning = 2;
+    let willpower = 1;
+    let precense = 3;
+    let xp = 90;
+
+  } else if (this.species === "chiss") {
+    let brawn = 2;
+    let agility = 2;
+    let intelligence = 3;
+    let cunning = 2;
+    let willpower = 2;
+    let precense = 1;
+    let xp = 100;
+
+  } else if (this.species === "duros") {
+    let brawn = 1;
+    let agility = 2;
+    let intelligence = 3;
+    let cunning = 2;
+    let willpower = 2;
+    let precense = 2;
+    let xp = 100;
+
+  } else if (this.species === "toydarian") {
+    let brawn = 1;
+    let agility = 1;
+    let intelligence = 2;
+    let cunning = 2;
+    let willpower = 3;
+    let precense = 3;
+    let xp = 90;
+
+  } else if (this.species === "drall") {
+    let brawn = 1;
+    let agility = 1;
+    let intelligence = 4;
+    let cunning = 2;
+    let willpower = 2;
+    let precense = 2;
+    let xp = 90;
+
+  } else if (this.species === "selonian") {
+    let brawn = 2;
+    let agility = 3;
+    let intelligence = 2;
+    let cunning = 1;
+    let willpower = 3;
+    let precense = 1;
+    let xp = 80;
+
+  } else if (this.species === "corellianHuman") {
+    let brawn = 2;
+    let agility = 2;
+    let intelligence = 2;
+    let cunning = 2;
+    let willpower = 2;
+    let precense = 2;
+    let xp = 110;
+
+  } else if (this.species === "hutt") {
+    let brawn = 3;
+    let agility = 2;
+    let intelligence = 2;
+    let cunning = 2;
+    let willpower = 3;
+    let precense = 2;
+    let xp = 70;
+
+  } else if (this.species === "gank") {
+    let brawn = 2;
+    let agility = 2;
+    let intelligence = 2;
+    let cunning = 2;
+    let willpower = 2;
+    let precense = 1;
+    let xp = 110;
+
+  } else if (this.species === "aqualish") {
+    let brawn = 3;
+    let agility = 2;
+    let intelligence = 1;
+    let cunning = 2;
+    let willpower = 2;
+    let precense = 2;
+    let xp = 90;
+
+  } else if (this.species === "klatooinian") {
+    let brawn = 2;
+    let agility = 3;
+    let intelligence = 2;
+    let cunning = 2;
+    let willpower = 1;
+    let precense = 2;
+    let xp = 100;
+
+  } else if (this.species === "weequay") {
+    let brawn = 3;
+    let agility = 2;
+    let intelligence = 1;
+    let cunning = 3;
+    let willpower = 2;
+    let precense = 1;
+    let xp = 90;
+
+  } else if (this.species === "arcona") {
+    let brawn = 1;
+    let agility = 2;
+    let intelligence = 2;
+    let cunning = 2;
+    let willpower = 3;
+    let precense = 2;
+    let xp = 100;
+
+  } else if (this.species === "chevin") {
+    let brawn = 3;
+    let agility = 1;
+    let intelligence = 2;
+    let cunning = 3;
+    let willpower = 2;
+    let precense = 1;
+    let xp = 80;
+
+  } else if (this.species === "gran") {
+    let brawn = 2;
+    let agility = 2;
+    let intelligence = 2;
+    let cunning = 1;
+    let willpower = 2;
+    let precense = 3;
+    let xp = 100;
+
+  } else if (this.species === "falleen") {
+    let brawn = 2;
+    let agility = 1;
+    let intelligence = 3;
+    let cunning = 2;
+    let willpower = 2;
+    let precense = 2;
+    let xp = 90;
+
+  } else if (this.species === "gotal") {
+    let brawn = 1;
+    let agility = 2;
+    let intelligence = 2;
+    let cunning = 2;
+    let willpower = 3;
+    let precense = 2;
+    let xp = 100;
+
+  } else if (this.species === "quarren") {
+    let brawn = 2;
+    let agility = 2;
+    let intelligence = 1;
+    let cunning = 2;
+    let willpower = 3;
+    let precense = 2;
+    let xp = 95;
+
+  } else if (this.species === "ithorian") {
+    let brawn = 2;
+    let agility = 1;
+    let intelligence = 2;
+    let cunning = 2;
+    let willpower = 3;
+    let precense = 2;
+    let xp = 90;
+
+  } else if (this.species === "monCalamari") {
+    let brawn = 2;
+    let agility = 2;
+    let intelligence = 3;
+    let cunning = 1;
+    let willpower = 2;
+    let precense = 2;
+    let xp = 100;
+
+  } else if (this.species === "sullustan") {
+    let brawn = 2;
+    let agility = 3;
+    let intelligence = 2;
+    let cunning = 1;
+    let willpower = 2;
+    let precense = 2;
+    let xp = 110;
+
+  } else if (this.species === "chadra-fan") {
+    let brawn = 1;
+    let agility = 3;
+    let intelligence = 3;
+    let cunning = 2;
+    let willpower = 1;
+    let precense = 2;
+    let xp = 90;
+
+  } else if (this.species === "dressellian") {
+    let brawn = 2;
+    let agility = 2;
+    let intelligence = 2;
+    let cunning = 2;
+    let willpower = 3;
+    let precense = 1;
+    let xp = 110;
+
+  } else if (this.species === "xexto") {
+    let brawn = 1;
+    let agility = 3;
+    let intelligence = 2;
+    let cunning = 2;
+    let willpower = 1;
+    let precense = 2;
+    let xp = 85;
+
+  } else if (this.species === "cerean") {
+    let brawn = 2;
+    let agility = 1;
+    let intelligence = 3;
+    let cunning = 2;
+    let willpower = 2;
+    let precense = 2;
+    let xp = 90;
+
+  } else if (this.species === "kelDor") {
+    let brawn = 1;
+    let agility = 2;
+    let intelligence = 2;
+    let cunning = 2;
+    let willpower = 3;
+    let precense = 2;
+    let xp = 100;
+
+  } else if (this.species === "mirialan") {
+    let brawn = 2;
+    let agility = 3;
+    let intelligence = 2;
+    let cunning = 1;
+    let willpower = 2;
+    let precense = 2;
+    let xp = 100;
+
+  } else if (this.species === "nautolan") {
+    let brawn = 3;
+    let agility = 2;
+    let intelligence = 2;
+    let cunning = 2;
+    let willpower = 1;
+    let precense = 2;
+    let xp = 100;
+
+  } else if (this.species === "togruta") {
+    let brawn = 1;
+    let agility = 2;
+    let intelligence = 2;
+    let cunning = 3;
+    let willpower = 2;
+    let precense = 2;
+    let xp = 100;
+
+  } else if (this.species === "zabrak") {
+    let brawn = 2;
+    let agility = 2;
+    let intelligence = 2;
+    let cunning = 2;
+    let willpower = 3;
+    let precense = 1;
+    let xp = 100;
+  }
+}
+}
